17bade0296b49cbd7524471f8a2ad3744000405e
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-24 07:59:46 -0400

    2000

--- atividade-3-ano-bissexto.js
+++ atividade-3-ano-bissexto.js
@@ -3,9 +3,9 @@ deve ser divisivel por 4 mas Não divisivel por 100
 OU divisivel por 100 e por 400
 */
 
-const anoBissexto = Number(2004)
+const anoBissexto = Number(2000)
 
-if ((anoBissexto % 4 === 0) && (anoBissexto % 100 !== 0) || (anoBissexto % 100 === 0) && (anoBissexto % 400 === 0)) {
+if (((anoBissexto % 4 === 0) && (anoBissexto % 100 !== 0)) || anoBissexto % 400 === 0) {
     console.log('É ano Bissexto')
 } else {
     console.log('Não é bissexto')
